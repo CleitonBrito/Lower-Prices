beaefa1bfebee89614290420da05084814a6ef46
CleitonBrito <cleytonbritto3003@gmail.com>
2022-10-17 13:46:50 -0300

    update Model Produtos

--- app/Models/Produtos.php
+++ app/Models/Produtos.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Mercados;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Produtos extends Model
 {
-    //
+    use Notifiable;
+    private $produto;
+
+    public function __construct(Produto $produto){
+        $this->produto = $produto;
+    }
+
+    public function mercado(){
+        return $this->belongsTo(Mercado::class, 'fk_id_mercado');
+    }
 }
