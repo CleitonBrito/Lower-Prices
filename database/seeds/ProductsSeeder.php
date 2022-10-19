2d733d0f9851ae4780dc1c94f0132fd4b6666aff
CleitonBrito <cleytonbritto3003@gmail.com>
2022-10-19 20:25:45 -0300

    update Seeders

---
+++ database/seeds/ProductsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Markets;
+use App\Models\Products;
+use Illuminate\Database\Seeder;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Products::class, 3)->create();
+
+    }
+}
