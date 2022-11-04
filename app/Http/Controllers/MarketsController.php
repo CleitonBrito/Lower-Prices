34c3f9b5d27dc26abb2d5548bd05026061ff73ca
CleitonBrito <cleytonbritto3003@gmail.com>
2022-11-04 10:57:46 -0300

    update MarketController

--- app/Http/Controllers/MarketsController.php
+++ app/Http/Controllers/MarketsController.php
@@ -8,77 +8,45 @@ use Illuminate\Http\Request;
 
 class MarketsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $markets;
+    public function __construct(Markets $markets){
+        $this->markets = $markets;
+    }
+
     public function index()
     {
         return view('site.market');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Mercados  $mercados
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Mercados $mercados)
+    public function show($id)
     {
-        //
+        $market = $this->markets::find($id);
+        $market->products;
+
+        return view('site.market', ['data' => $market]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Mercados  $mercados
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit(Mercados $mercados)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Mercados  $mercados
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Mercados $mercados)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Mercados  $mercados
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Mercados $mercados)
     {
         //
