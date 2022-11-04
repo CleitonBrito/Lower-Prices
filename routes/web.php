9d9fd5acd54e5c00a8849bd151275c8962a0ca6e
CleitonBrito <cleytonbritto3003@gmail.com>
2022-11-04 10:58:32 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/market', 'MarketsController@index');
+Route::get('/market/{id_market}', 'MarketsController@show');
