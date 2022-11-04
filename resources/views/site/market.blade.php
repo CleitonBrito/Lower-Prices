69e7abb2a9a7d0b17017a39655f3ad6db6d8af0a
CleitonBrito <cleytonbritto3003@gmail.com>
2022-11-04 10:58:22 -0300

    update view market

--- resources/views/site/market.blade.php
+++ resources/views/site/market.blade.php
@@ -150,40 +150,38 @@
 
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $data['name'] }}</h1>
         </div>
     </header>
     <main>
         <div class="bg-white">
             <div class="mx-auto max-w-2xl py-0 px-4 sm:py-6 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 class="text-2xl tracking-tight text-gray-500">All Markets</h2>
+                <h2 class="text-2xl tracking-tight text-gray-500">All Products</h2>
 
                 <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    @for($x = 0; $x < 50; $x++)
-                    <div class="group relative">
-                        <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                    @foreach($data['products'] as $product)
+                    <div class="group relative bg-gray-200 p-2 rounded">
+                        <div class="h-20 w-20 rounded-md bg-gray-500 group-hover:opacity-75">
                         @php
                             $random = rand(1, 4);
                             $markets = array("Comercial Barbosa", "Lara Super", "Atakarejo", "Supermercado São Jorge");
                         @endphp
-                        <img src="<?php echo "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-0". $random .".jpg" ?>" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                        <img src="<?php echo "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-0". $random .".jpg" ?>" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center h-full w-full">
                         </div>
                         <div class="mt-4 flex justify-between">
                         <div>
                             <h3 class="text-lg text-gray-700">
                             <a href="#">
                                 <span aria-hidden="true" class="absolute inset-0"></span>
-                                @php echo $markets[array_rand($markets)] @endphp
+                                @php echo $product->name @endphp
                             </a>
-                            <p class="mt-1 text-sm text-gray-500">Centro - Filadélfia-BA</p>
+                            <p class="mt-1 text-sm text-gray-500">{{ $product->description }}</p>
                             </h3>
                         </div>
                         </div>
                     </div>
-                    @endfor
-                    
+                    @endforeach
 
-                <!-- More products... -->
                 </div>
             </div>
             </div>
