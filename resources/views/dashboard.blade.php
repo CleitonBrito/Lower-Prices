4e232d4e79b84f46684ee403a63d3935e3806531
CleitonBrito <cleytonbritto3003@gmail.com>
2022-11-14 22:04:39 -0300

    update image markets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,7 +43,7 @@
                         <div class="group relative pt-1 px-3 rounded-md shadow-xl bg-gradientGray-100">
                             <div class="min-h-30 max-h-60 flex flex-column aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
                                 @if(isset($market->image->path))
-                                    <img src="{{ url('storage/'.$market->image->path) }}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                                    <img src="{{ url('storage/'.$market->image->path) }}" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                                 @else
                                     <h3 class="d-flex justify-content-center align-items-center w-full h-full m-0 text-center text-gray-500">{{ $market->name }}</h3>
                                 @endif
